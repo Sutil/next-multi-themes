dfbcc7d71d00bacdf07d1f15d70df5db771384c0
Eduardo Sutil <eduardo.sutil@koho.ca>
2023-08-28 10:18:09 -0300

    Initial commit

---
+++ examples/example/pages/index.js
@@ -0,0 +1,39 @@
+import { useTheme } from 'next-themes'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+const Index = () => {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
+
+  return (
+    <div>
+      <h1>next-themes Example</h1>
+      <select value={theme} onChange={e => setTheme(e.target.value)} data-test-id='theme-selector'>
+        <option value="system">System</option>
+        {mounted && (
+          <>
+            <option value="dark">Dark</option>
+            <option value="light">Light</option>
+          </>
+        )}
+      </select>
+
+      <br />
+      <br />
+
+      <div>
+        <Link href="/dark">
+          <a>Forced Dark Page</a>
+        </Link>{' '}
+        •{' '}
+        <Link href="/light">
+          <a>Forced Light Page</a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Index
